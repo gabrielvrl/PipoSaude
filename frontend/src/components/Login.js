f8cbccd6ab4c4d0858085a11c37854a90a068c8e
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-10 10:29:36 -0300

    Login Component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import './Login.css'
+import './style.css'
+
+import { AiFillGoogleCircle, AiFillFacebook } from 'react-icons/ai'
 
 function Login(){
+    function handleGoogleLogin(e){
+        e.preventDefault()
+        console.log("Google Login")
+    }
+
+    function handleFacebookLogin(e){
+        e.preventDefault()
+        console.log("Facebook Login")
+    }
+
     return(
-        <div className="login-container">
-            Login
+        <div className="background-container">
+            <div className="ui-container">
+                <div className="login-container">
+                    <h1>Login</h1>
+                    <div>
+                        <AiFillGoogleCircle style={{ fontSize:75, marginRight: '3px'}} onClick={handleGoogleLogin} />
+                        <AiFillFacebook style={{ fontSize:75}} onClick={handleFacebookLogin} />
+                    </div>
+                    <h1>ou</h1>
+                    <div>
+                        <h2>Email:</h2>
+                        <input className="login-input" />
+                        <h2>Senha:</h2>
+                        <input className="login-input" />
+                    </div>
+                    <button className="login-button">Submit</button>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './components/Login';
 
-
 export default function Routes() {
     return (
         <BrowserRouter>
