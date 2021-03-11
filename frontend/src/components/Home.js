ff5b1b6fc990b07d1a5df94004b2c69f286404a9
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-11 11:00:35 -0300

    Adding Home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Login.css'
 
 function Home(){
+    const name = 'Tio Patinhas Bank'
     return(
-        <div>
-            Home
+        <div className="background-container">
+            <div className="ui-container">
+                <div className="home-container">
+                    <h1>Seja Bem-vindo, {name} </h1>
+                    <div>
+                        <h2>Esses</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
