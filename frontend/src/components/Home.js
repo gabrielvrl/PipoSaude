4fc41d1bf67d32a97b0aead284737285b48ac43a
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-11 20:29:54 -0300

    Home changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import './Login.css'
+import './Home.css'
+
+import { AiFillPlusCircle } from 'react-icons/ai'
 
 function Home(){
     const name = 'Tio Patinhas Bank'
+    const employeesNumber = "16"
+
+    function handleClick(e){
+        e.preventDefault()
+        console.log("push add new employer")
+    }
+
     return(
-        <div className="background-container">
-            <div className="ui-container">
-                <div className="home-container">
-                    <h1>Seja Bem-vindo, {name} </h1>
-                    <div>
-                        <h2>Esses</h2>
-                    </div>
-                </div>
+        <div className="home-container">
+            <div className="home-header">
+                <h1>Olá, </h1><h1 id="home-name-client">{name}</h1>
+            </div>
+            <div className="home-infos">
+                <h3>Você tem {employeesNumber} funcionários atualmente</h3>
+            </div>
+            <div className="home-add-new-employee">
+                <AiFillPlusCircle id="add-button" /> <h3>Adicionar novo funcionário</h3>
             </div>
         </div>
     )
