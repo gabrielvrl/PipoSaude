d5a916a6a28bcfe96af8a9ed179b827647e24c33
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-11 11:01:30 -0300

    Routes refac

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './components/Header'
 import Login from './components/Login';
 import SignUp from './components/SignUp'
 import Home from './components/Home'
+import Footer from './components/Footer'
 
 export default function Routes() {
     return (
         <BrowserRouter>
+            <Header />
             <Route path="/" exact component={Login} />
             <Route path="/signup" component={SignUp} />
             <Route path="/home" component={Home} />
+            <Footer />
         </BrowserRouter>
     );
 }
